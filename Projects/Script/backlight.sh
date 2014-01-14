fecc20e26fcca07f6e6c737072a3f91d1f1eb9b8
ashik992 <ashik.992@gmail.com>
2014-01-14 11:18:16 +0600

    Fixed exceptions in shutdown defaultw

--- Projects/Script/backlight.sh
+++ Projects/Script/backlight.sh
@@ -1,5 +1,12 @@
-sudo apt-get install xbacklight
-sudo sed -i '$ixbacklight | cat > ~/.brightness' /etc/gdm/PostSession/Default
+sudo -s
+
+apt-get install xbacklight
+ 
+mkdir -p /etc/gdm/PostSession
+touch /etc/gdm/PostSession/Default
+chmod +x /etc/gdm/PostSession/Default
+sed -i '$ixbacklight | cat > ~/.brightness' /etc/gdm/PostSession/Default
+
 touch ~/.gnomerc
 echo 'b=$(cat ~/.brightness)' >> ~/.gnomerc
 echo 'xbacklight -set $b' >> ~/.gnomerc
