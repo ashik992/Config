716cf501ec73aca9768a51d949ad62dd1919d5b7
ashik992 <ashik.992@gmail.com>
2014-01-14 11:20:52 +0600

    Comments on backlight

--- Projects/Script/backlight.sh
+++ Projects/Script/backlight.sh
@@ -1,12 +1,15 @@
 sudo -s
 
+#install xbacklight
 apt-get install xbacklight
  
+#edit shutdown default to remember brightness
 mkdir -p /etc/gdm/PostSession
 touch /etc/gdm/PostSession/Default
 chmod +x /etc/gdm/PostSession/Default
 sed -i '$ixbacklight | cat > ~/.brightness' /etc/gdm/PostSession/Default
 
+#edit startup default to impliment remembered brightness
 touch ~/.gnomerc
 echo 'b=$(cat ~/.brightness)' >> ~/.gnomerc
 echo 'xbacklight -set $b' >> ~/.gnomerc
